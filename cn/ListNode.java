acbb438e1b1515f7a20b9117f0e1c43cc06833a1
gamigo <gamigoy@gmail.com>
2020-03-05 23:02:46 +0800

    21. Merge Two Sorted Lists

--- cn/ListNode.java
+++ cn/ListNode.java
@@ -3,7 +3,12 @@ package cn;
 /**
  * @author Gamigo
  * @create 2020/3/5 22:50
- * @description
+ * @description Definition for singly-linked list.
  */
 public class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
 }
+
--- cn/MergeTwoSortedLists.java
+++ cn/MergeTwoSortedLists.java
@@ -3,7 +3,34 @@ package cn;
 /**
  * @author Gamigo
  * @create 2020/3/5 22:49
- * @description
+ * @description 21. Merge Two Sorted Lists
  */
 public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode p = new ListNode(-1);
+        ListNode result = p;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+                p = p.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+                p = p.next;
+            }
+        }
+        if (l1 != null) {
+            p.next = l1;
+        } else {
+            p.next = l2;
+        }
+        return result.next;
+    }
 }
